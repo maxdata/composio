961f15ea9b651fd1ca3a570cd8943a3d183df67c
MaxData <max@workhere.org>
2024-09-17 20:27:10 -0400

    c

--- AUTHORS.md
+++
@@ -1,6 +0,0 @@
-# Authors
-
-* [Utkarsh Dixit](https://github.com/utkarsh-dixit) <utkarsh@composio.dev>
-* [Sawardip Saha](https://github.com/sawradip) <swaradip@composio.dev>
-* [Soham Ganatra](https://github.com/sohamganatra) <soham@composio.dev>
-* [Karan Vaidya](https://github.com/kaavee315) <karan@composio.dev>
--- js/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Composio } from "./sdk";
-import { LangchainToolSet } from "./frameworks/langchain";
-import { OpenAIToolSet } from "./frameworks/openai";
-import { CloudflareToolSet } from "./frameworks/cloudflare";
-import { VercelAIToolSet } from "./frameworks/vercel";
-import { Workspace } from "./env/";
-
-export { Composio, LangchainToolSet, OpenAIToolSet, CloudflareToolSet, VercelAIToolSet, Workspace };
